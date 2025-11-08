e1ac0afb3d023b1752fdd691dab17656d1d5ff38
Junayed <junayed@example.com>
2025-11-09 01:04:43 +0600

    added text

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero = () => {
     return (
         <div>
-            <div className="h-36 border border-fuchsia-800">this is hero</div>
+            <div className="h-36 border border-fuchsia-800">this is hero section </div>
         </div>
     );
 };
